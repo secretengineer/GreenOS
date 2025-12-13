a72fb4e2be3e36bbac27a2c1375174d4f24d5a4d
Patrick Ryan <secretengineer@gmail.com>
2025-12-13 10:09:12 -0700

    add Google Analytics script injection to Vite configuration

--- WebUI/vite.config.js
+++ WebUI/vite.config.js
@@ -1,8 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Google Analytics script to inject
+const googleAnalyticsScript = `
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-JY0JWSR0T8"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-JY0JWSR0T8');
+  </script>
+`;
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-google-analytics',
+      transformIndexHtml(html) {
+        return html.replace('<head>', `<head>${googleAnalyticsScript}`);
+      },
+    },
+  ],
   server: {
     port: 3000,
   },
